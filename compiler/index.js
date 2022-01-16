5a24b50808df6136329a82a1b0d2fcc39673cda4
nutella <nutell@4ka.com>
2022-01-16 17:59:26 -0500

    fix: imports

--- compiler/index.js
+++ compiler/index.js
@@ -1,13 +1,13 @@
 import { tokenize } from "./tokenizer.js";
 import { AST } from "./ast.js";
 import { generate } from "./generator.js";
-import { transpile } from "../assembler/transpiler.js";
+import { compile as asmCompiler } from "../assembler/index.js";
 
 export function compile(programSource) {
   const tokens = tokenize(programSource);
   const ast = new AST(tokens);
   const asmCode = generate(ast.body);
-  const machineCode = transpile(asmCode);
+  const machineCode = asmCompiler(asmCode);
 
   return machineCode;
 }
